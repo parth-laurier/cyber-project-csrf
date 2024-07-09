c3fe925bd746b585314b3266fa13aa82a5c4f470
prometheus0329 <guneetsingh0329@gmail.com>
2024-07-09 18:26:03 -0400

    Attck

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/session/new', function (req, res) {
   console.log("3");
   req.session.isValid = true;
   req.session.username = 'Parth';
-  req.session.email = 'psshah0411@gmail.com';
+  req.session.email = 'sid.alpha13@gmail.com';
   res.redirect('/');
 });
 
